3f1d89d660ad98e8c8a4bdd5fea3f91bc6f7907d
teerawat.th <teerawat.th@baac.or.th>
2018-07-17 16:40:34 +0700

    [Edited]: เพิ้มฟังก์ชัน CheckNumber กับ TestCheckNumber

--- player_test.go
+++ player_test.go
@@ -52,3 +52,29 @@ func Test_NewPlayer_Input_Name_B_With_Ticket_Should_Be_Player_Name_B_With_Ticket
 		t.Errorf("expected player is %v but it got %v", expectedPlayer, actualPlayer)
 	}
 }
+
+func Test_CheckNumber_Input_9_Should_Be_2_1(t *testing.T) {
+	number := 9
+	expectedPositionX := 2
+	expectedPositionY := 1
+	player := Player{
+		Name: "A",
+		Ticket: Ticket{
+			SizeX: 5,
+			SizeY: 5,
+			Grid: [][]State{
+				[]State{State{Number: 1}, State{Number: 17}, State{Number: 35}, State{Number: 51}, State{Number: 74}},
+				[]State{State{Number: 9}, State{Number: 21}, State{Number: 41}, State{Number: 38}, State{Number: 79}},
+				[]State{State{Number: 2}, State{Number: 23}, State{Number: 0, Status: true}, State{Number: 47}, State{Number: 68}},
+				[]State{State{Number: 14}, State{Number: 29}, State{Number: 32}, State{Number: 49}, State{Number: 66}},
+				[]State{State{Number: 11}, State{Number: 30}, State{Number: 39}, State{Number: 56}, State{Number: 70}},
+			},
+		},
+	}
+	x, y := player.CheckNumber(number)
+
+	if expectedPositionX != x || expectedPositionY != y {
+		t.Errorf("expected Position is %v,%v but got %v,%v", expectedPositionX, expectedPositionY, x, y)
+	}
+
+}
